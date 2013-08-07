6d380189359bb8d189f835ca813e6a2751dafb4b
Techvalens <techvalens@Techvalenss-Mac-mini-2.local>
2013-08-07 10:57:35 +0530

    all

--- WeLiiKe/AppDelegate.h
+++ WeLiiKe/AppDelegate.h
@@ -7,9 +7,46 @@
 //
 
 #import <UIKit/UIKit.h>
+#import<MapKit/MapKit.h>
+//#import <FacebookSDK/FacebookSDK.h>
+#import <Accounts/Accounts.h>
+#import <Twitter/Twitter.h>
+#import "FBConnect.h"
+#import "WeLiikeWebService.h"
+#define kSocialAccountTypeKey @"SOCIAL_ACCOUNT_TYPE"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@class ACAccount;
 
-@property (strong, nonatomic) UIWindow *window;
+@interface AppDelegate : UIResponder <UIApplicationDelegate,CLLocationManagerDelegate,MKMapViewDelegate,FBLoginViewDelegate,UITabBarControllerDelegate>{
+
+    UINavigationController *navControllerApp;
+    UITabBarController *tabBarController;
+    NSMutableDictionary *dictionaryForImageCacheing;
+    UIButton *btnPost;
+    NSMutableArray *arrayOfEmailContact;
+    NSMutableArray *arrayOfUserForMessage;
+    
+    //*********
+    //CLLocationManager *locationManager;
+    double currentLatitude;
+    double currentLongitute;
+    
+    Facebook *facebook;
+    NSString *strForAddressDelegate;
+    int countForAddress;
+}
+@property (nonatomic, retain)NSString *strForAddressDelegate;
+@property (nonatomic, retain) Facebook *facebook;
+@property(assign,nonatomic)double currentLatitude;
+@property(assign,nonatomic)double currentLongitute;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property(nonatomic,retain)NSMutableArray *arrayOfEmailContact,*arrayOfUserForMessage;
+@property(nonatomic,retain)UIButton *btnPost;
+@property(nonatomic,retain) NSMutableDictionary *dictionaryForImageCacheing;
+@property(nonatomic,retain)IBOutlet UINavigationController *navControllerApp;
+@property(nonatomic,retain)IBOutlet UITabBarController *tabBarController;
+@property (strong, nonatomic)IBOutlet UIWindow *window;
+
+- (void)getTwitterAccountOnCompletion:(void(^)(ACAccount *))completionHandler;
 
 @end
